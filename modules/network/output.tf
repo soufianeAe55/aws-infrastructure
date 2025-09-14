9dc29d5a816777f09a127dbee1200d5170256aee
soufianeAe55 <boubellasoufiane@gmail.com>
2025-09-14 22:23:49 +0200

    vpc creation

--- modules/network/output.tf
+++ modules/network/output.tf
@@ -2,16 +2,16 @@ output "vpc_id" {
   value       = aws_vpc.vpc.id
   description = "The ID of the VPC"
 }
-
+output "igw_id" {
+  value       = aws_internet_gateway.gw.id
+  description = "The ID of the internet gateway"
+}
 output "public_subnet_id" {
   value       = aws_subnet.subnets["public_subnet"].id
   description = "The ID public subnet"
 }
-
 output "private_subnet_id" {
   value       = aws_subnet.subnets["private_subnet"].id
   description = "The ID private subnet"
 }
-
-
 
