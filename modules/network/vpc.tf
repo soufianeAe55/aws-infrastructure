7f6276c38a8ac97476f19ce2448206346e00c806
soufianeAe55 <boubellasoufiane@gmail.com>
2025-09-14 15:45:09 +0200

    vpc creation

--- modules/network/vpc.tf
+++ modules/network/vpc.tf
@@ -1,10 +1,12 @@
 resource "aws_vpc" "vpc" {
+
+  name             =
   cidr_block       = local.vpc_cidr
 
   region          = "eu-west-3"
 
   tags = {
-    Name = "main",
+    Name = "soufiane-vnet",
     Env = "DEV",
     CREATED_BY ="SOUFIANE"
   }
