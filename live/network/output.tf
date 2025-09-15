39a64676846204ff81e0a38b9b021b0682e13aa6
soufianeAe55 <boubellasoufiane@gmail.com>
2025-09-16 00:40:35 +0200

    vpc creation

--- live/network/output.tf
+++ live/network/output.tf
@@ -2,3 +2,15 @@ output "vpc_id" {
   value       = module.vpc.vpc_id
   description = "The ID of the VPC"
 }
+output "igw_id" {
+  value       = module.vpc.igw_id
+  description = "The ID of the internet gateway"
+}
+output "public_subnet_ids" {
+  value       = module.vpc.public_subnet_ids
+  description = "The ID public subnet"
+}
+output "private_subnet_ids" {
+  value       = module.vpc.private_subnet_ids
+  description = "The ID private subnet"
+}
