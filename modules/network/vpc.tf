d7773c6ece5cebf818f26805e575b3221db9b416
soufianeAe55 <boubellasoufiane@gmail.com>
2025-09-15 21:26:45 +0200

    vpc creation

--- modules/network/vpc.tf
+++ modules/network/vpc.tf
@@ -4,6 +4,9 @@ resource "aws_vpc" "vpc" {
 
   region          = "eu-west-3"
 
+  private_subnets = ["10.0.10.0/24" ]
+  public_subnets  = ["10.0.101.0/24"]
+
   tags = {
     Name = "soufiane-vnet",
     Env = "DEV",
