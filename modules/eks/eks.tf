de3634f98de94579e547fb65c84d4e9fc8caa981
soufianeAe55 <boubellasoufiane@gmail.com>
2025-09-17 01:29:55 +0200

    vpc creation

--- modules/eks/eks.tf
+++ modules/eks/eks.tf
@@ -3,7 +3,7 @@ module "eks" {
   version = "~> 21.0"
 
   name               = "dev-cluster"
-  kubernetes_version = "1.33"
+  kubernetes_version = "1.30"
 
   # Optional
   endpoint_public_access = true
