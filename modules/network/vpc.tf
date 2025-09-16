3de3681174b0330cb434921e33df60fad38bcc3c
soufianeAe55 <boubellasoufiane@gmail.com>
2025-09-16 18:50:56 +0200

    vpc creation

--- modules/network/vpc.tf
+++ modules/network/vpc.tf
@@ -24,7 +24,7 @@ module "vpc" {
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
-  enable_nat_gateway     = false   # NAT for private subnets (so worker nodes can reach internet)
+  enable_nat_gateway     = yes   # NAT for private subnets (so worker nodes can reach internet)
   single_nat_gateway     = false   # Only one NAT to save cost
   enable_dns_hostnames   = true
   enable_dns_support     = true
