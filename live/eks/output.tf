d8f1116019053ecdf58b3d2d3ac5e0f800cc0450
soufianeAe55 <boubellasoufiane@gmail.com>
2025-09-17 02:41:08 +0200

    vpc creation

--- live/eks/output.tf
+++ live/eks/output.tf
@@ -2,3 +2,14 @@ output "eks_id" {
   value       = module.eks.cluster_id
   description = "The ID of the eks"
 }
+output "cluster_ca_cert" {
+  value = module.eks.cluster_ca_cert
+}
+
+output "cluster_name" {
+  value = module.eks.cluster_name
+}
+
+output "cluster_endpoint" {
+  value = module.eks.cluster_endpoint
+}
