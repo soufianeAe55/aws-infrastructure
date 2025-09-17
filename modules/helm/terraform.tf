210f9e78b8def37769e17f4d765c9bb6d177a457
soufianeAe55 <boubellasoufiane@gmail.com>
2025-09-17 02:57:46 +0200

    vpc creation

--- modules/helm/terraform.tf
+++ modules/helm/terraform.tf
@@ -1,7 +1,7 @@
 provider "helm" {
   kubernetes = {
     host                   = var.cluster_endpoint
-    cluster_ca_certificate = base64decode(var.cluster_ca_cert)
+    cluster_ca_certificate = base64decode(trimspace(var.cluster_ca_cert))
     exec = {
       api_version = "client.authentication.k8s.io/v1beta1"
       args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
